dcea9a7b265bf9410426886776554d20ddc3d0d9
zhouzheng <nothinchou@gmail.com>
2023-05-04 12:10:25 +0800

    Initial Commit

---
+++ ImagePro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImagePro
+//
+//  Created by coddmajes on 2023/5/4.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
